b4546d0cec7ee8a9ad87ac6940022eca0c314b12
Juan Maldonado <juan.maldonado@live.cl>
2025-02-13 17:00:49 -0300

    fix: added sanitized image name before saving file

--- backend/src/main/java/com/digitalhouse/turnos/service/ImageSavingService.java
+++ backend/src/main/java/com/digitalhouse/turnos/service/ImageSavingService.java
@@ -13,9 +13,15 @@ public class ImageSavingService {
 
     public String saveImage(MultipartFile file) throws IOException {
         String UPLOAD_DIR = "uploads/";
+        String originalName = file.getOriginalFilename();
+        String sanitizedName = originalName != null
+                ? originalName.replaceAll("[^a-zA-Z0-9.\\-_]", "-")
+                : null;
 
+        // ver como crear una carpeta de images para cada vehiculo
+//        Files.createDirectories(Path.of(UPLOAD_DIR + ))
         Files.createDirectories(Path.of(UPLOAD_DIR));
-        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        String fileName = UUID.randomUUID() + "_" + sanitizedName;
         Path filePath = Path.of(UPLOAD_DIR + fileName);
         Files.write(filePath, file.getBytes());
         return fileName;
