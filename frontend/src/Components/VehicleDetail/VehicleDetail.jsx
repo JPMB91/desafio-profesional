cb9363c4f4a9adb21229b104fc6dc5d3144dfa2c
Juan Maldonado <juan.maldonado@live.cl>
2025-03-08 10:08:36 -0300

    feat:(<ShareBar/>): Add share content links

--- frontend/src/Components/VehicleDetail/VehicleDetail.jsx
+++ frontend/src/Components/VehicleDetail/VehicleDetail.jsx
@@ -1,101 +1,3 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import { useNavigate, useParams } from "react-router-dom";
-// import styles from "./Detail.module.css";
-// import { ImageGallery } from "../ImageGallery/ImageGallery";
-// import { CharacteristicsDisplay } from "../CharacteristicsDisplay/CharacteristicsDisplay.jsx";
-// import { ReservationCalendar } from "../ReservationCalendar/ReservationCalendar.jsx";
-
-
-// export const VehicleDetail = () => {
-//   const [vehicleData, setVehicleData] = useState({});
-//   const [characteristic, setCharacteristics] = useState([]);
-//   const [showModal, setShowModal] = useState(false);
-//   const [error, setError] = useState(null);
-//   const navigate = useNavigate();
-//   const { id } = useParams();
-
-//   useEffect(() => {
-//     const getDetail = async () => {
-//       try {
-//         const response = await axios.get(
-//           `http://localhost:8080/api/vehicles/${id}`
-//         );
-//         setVehicleData(response.data);
-//         const { characteristics } = response.data;
-//         setCharacteristics(characteristics);
-//         console.log(characteristics);
-//       } catch (error) {
-//         console.log("Error obteniendo detalle del vehiculo: ", error);
-//         setError("No se pudo cargar la información del vehículo.");
-//       }
-//     };
-//     getDetail();
-//   }, [id]);
-
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//   }, []);
-
-//   if (error) return <p style={{ color: "red" }}>{error}</p>;
-//   if (!vehicleData) return <p>Cargando detalles del vehículo...</p>;
-
-//   return (
-//     <div className={styles.detailContainer}>
-//       <div className={styles.detailHeader}>
-//         <h2 className={styles.title}>
-//           {vehicleData.brand} {vehicleData.model}
-//         </h2>
-//         <div>
-//           <button className={styles.backButton} onClick={() => navigate(-1)}>
-//             ← Volver
-//           </button>
-//         </div>
-//       </div>
-
-//       <p className={styles.description}>{vehicleData.description}</p>
-
-//       {vehicleData.images && vehicleData.images.length > 0 && (
-//         <div className={styles.gallery}>
-//           <div className={styles.mainImage}>
-//             <img
-//               src={`http://localhost:8080/api/vehicles/uploads/${vehicleData.images[0].filename}`}
-//               alt="Vehiculo"
-//             />
-//           </div>
-
-//           <div className={styles.imageGrid}>
-//             {vehicleData.images.slice(1, 5).map((img, index) => (
-//               <img
-//                 key={index}
-//                 src={`http://localhost:8080/api/vehicles/uploads/${img.filename}`}
-//                 alt={`Vehiculo ${index + 1}`}
-//               />
-//             ))}
-
-//             <button
-//               className={styles.viewMore}
-//               onClick={() => setShowModal(true)}
-//             >
-//               Ver más
-//             </button>
-//           </div>
-
-//           {showModal && (
-//             <ImageGallery
-//               images={vehicleData.images}
-//               close={() => setShowModal(false)}
-//               showModal={showModal}
-//             />
-//           )}
-//         </div>
-//       )}
-//       <CharacteristicsDisplay characteristics={characteristic} />
-
-//       <ReservationCalendar id={id}/>
-//     </div>
-//   );
-// };
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -103,14 +5,20 @@ import { ImageGallery } from "../ImageGallery/ImageGallery";
 import { CharacteristicsDisplay } from "../CharacteristicsDisplay/CharacteristicsDisplay.jsx";
 import { ReservationCalendar } from "../ReservationCalendar/ReservationCalendar.jsx";
 import Politicas from "../Politicas/Politicas.jsx";
+import { ShareBar } from "../ShareBar/ShareBar.jsx";
+import { FacebookIcon, FacebookShareButton } from "react-share";
 
 export const VehicleDetail = () => {
   const [vehicleData, setVehicleData] = useState({});
   const [characteristic, setCharacteristics] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [error, setError] = useState(null);
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
   const navigate = useNavigate();
   const { id } = useParams();
+
+  const url = window.location.href;
 
   useEffect(() => {
     const getDetail = async () => {
@@ -120,8 +28,14 @@ export const VehicleDetail = () => {
         );
         setVehicleData(response.data);
         const { characteristics } = response.data;
-        setCharacteristics(characteristics);
 
+        setCharacteristics(characteristics);
+        setTitle(
+          `Encontré este increible ${response.data.name} en Aurora Motors`
+        );
+        setImage(
+          `http://localhost:8080/api/vehicles/uploads/${response.data.images[0]}.filename`
+        );
       } catch (error) {
         console.log("Error obteniendo detalle del vehiculo: ", error);
         setError("No se pudo cargar la información del vehículo.");
@@ -134,7 +48,13 @@ export const VehicleDetail = () => {
     window.scrollTo(0, 0);
   }, []);
 
-  if (error) return <p className="text-red-500 text-3xl font-bold text-center p-8">{error}</p>;
+  const useLocation = () => {
+    console.log(window.location.href);
+  };
+  if (error)
+    return (
+      <p className="text-red-500 text-3xl font-bold text-center p-8">{error}</p>
+    );
   if (!vehicleData) return <p>Cargando detalles del vehículo...</p>;
 
   return (
@@ -144,7 +64,10 @@ export const VehicleDetail = () => {
           {vehicleData.brand} {vehicleData.model}
         </h2>
         <div>
-          <button className="bg-none border-none text-lg cursor-pointer font-bold" onClick={() => navigate(-1)}>
+          <button
+            className="bg-none border-none text-lg cursor-pointer font-bold"
+            onClick={() => navigate(-1)}
+          >
             ← Volver
           </button>
         </div>
@@ -175,7 +98,7 @@ export const VehicleDetail = () => {
             <button
               className="text-white font-bold py-2 px-4 border-none cursor-pointer absolute bottom-2.5 right-2.5"
               onClick={() => setShowModal(true)}
-              style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
             >
               Ver más
             </button>
@@ -192,9 +115,21 @@ export const VehicleDetail = () => {
       )}
       <CharacteristicsDisplay characteristics={characteristic} />
 
-      <ReservationCalendar id={id}/>
+      <ReservationCalendar id={id} />
+      <ShareBar
+        image={
+          vehicleData.images?.[0]?.filename
+            ? `http://localhost:8080/api/vehicles/uploads/${vehicleData.images[0].filename}`
+            : ""
+        }
+        title={title}
+        url={url}
+        description={title}
+      />
 
       <Politicas />
+
+      
     </div>
   );
 };
