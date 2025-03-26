b512a7c9ffe86a566b0aabe768ea47df66a31f00
Juan Maldonado <juan.maldonado@live.cl>
2025-03-26 11:39:37 -0300

    feat:(EmailService): Add @Async annotation for faster email sending processing

--- backend/src/main/java/com/digitalhouse/turnos/RentApplication.java
+++ backend/src/main/java/com/digitalhouse/turnos/RentApplication.java
@@ -2,8 +2,10 @@ package com.digitalhouse.turnos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class RentApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/digitalhouse/turnos/service/EmailService.java
+++ backend/src/main/java/com/digitalhouse/turnos/service/EmailService.java
@@ -3,10 +3,13 @@ package com.digitalhouse.turnos.service;
 import com.digitalhouse.turnos.entity.Reservation;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +19,8 @@ public class EmailService {
 
     @Value("${spring.mail.username}")
     private String fromEmail;
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
 
     public void sendHtmlEmail(String to, String subject, String htmlBody) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
@@ -30,7 +35,7 @@ public class EmailService {
     }
 
 
-
+    @Async
     public void sendReservationEmail(Reservation reservation) {
 
         try {
@@ -40,12 +45,12 @@ public class EmailService {
             sendHtmlEmail(to,subject,htmlBody);
         }catch (MessagingException e  ){
             System.err.println("Error al enviar correo de confirmación de reserva: "+e);
+            logger.error("Error al enviar correo de confirmación de reserva: "+e);
         }
 
     }
 
     private String reservationEmailBody(Reservation reservation) {
-
         return String.format(
                 "<html>" +
                         "<head>" +
@@ -65,6 +70,7 @@ public class EmailService {
                         "    <li><strong>Número de Reserva:</strong> %s</li>" +
                         "    <li><strong>Fecha de Inicio:</strong> %s</li>" +
                         "    <li><strong>Fecha de Término:</strong> %s</li>" +
+                        "    <li><strong>Ubicación:</strong> Aeropuerto Internacional Arturo Merino Benítez, Armando Cortinez Ote. 1704, Pudahuel, Región Metropolitana, Chile</li>" +
                         "  </ul>" +
                         "  <p>Gracias por confiar en Aurora Motors.</p>" +
                         "  <p>Teléfono de contacto: +569 33691660.</p>" +
